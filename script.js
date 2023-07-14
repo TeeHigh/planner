a0d0140465b1bfdf755b5239c5b0e7990d9800bc
Taiwo Ifeoluwase <105116838+TeeHigh@users.noreply.github.com>
2023-07-14 20:47:54 +0100

    Update script.js
    Function to close sidebar on click outside

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let open = document.getElementById("openBtn");
 let ul = document.getElementById("ul");
 let close = document.querySelector("#closeBtn")
 let li = document.querySelectorAll(".line")
+let sub = document.querySelector("submit-btn")
 
 open.addEventListener("click", function(){
     ul.style.right = "0"
@@ -10,8 +11,18 @@ close.addEventListener("click", function(){
     ul.style.right = "-300px"
 })
 
+document.onclick = function(e){
+    if(e.target.id !== 'openBtn' && e.target.id !== 'ul'){
+        ul.style.right = "-300px"
+    }
+}
+
 li.forEach( item => {
     item.addEventListener('click', function(){
         item.classList.add('underline')
     })
+})
+
+sub.addEventListener("click", function(){
+  location.reload()
 })
